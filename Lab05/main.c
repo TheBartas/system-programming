5fbc7de300fd951e2e3cf564c4f0e1d66949feb0
Bartek <bartekszewczyk2003@gmail.com>
2025-03-29 03:32:19 +0100

    Lab 05 zrobione

---
+++ Lab05/main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+// w - czas między tworzeniem nowych procesów
+// m - maksymalny czas życia procesu
+
+bool loop = true;
+// int number_of_process = 0;
+volatile sig_atomic_t number_of_process = 0;
+
+void sigchild_handler(int sigNo, siginfo_t *si, void *sc) {
+    time_t t; 
+    struct tm *_tm;
+    number_of_process--;
+    time(&t);
+    _tm = localtime(&t);
+
+    printf("\t\t[ %d ] [ %d ] [ %02d / %02d / %04d %02d:%02d:%02d ]\n", si->si_pid, si->si_status,
+            _tm->tm_mday, _tm->tm_mon + 1, _tm->tm_year + 1900, _tm->tm_hour, _tm->tm_min, _tm->tm_sec
+        );
+}
+
+void sigalarm_handler(int sigNo, siginfo_t *si, void *sc) {
+    exit(3); 
+}
+
+void sigint_handler(int sigNo, siginfo_t *si, void *sc) {
+    loop = false;
+}
+
+unsigned long long inf_factorial() {
+    unsigned long long factorial = 1;
+
+    for (int i = 1;;i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main(int argc, char* argv[]) {
+    int w_flag = 0, m_flag = 0;
+    int ret, w, m;
+    const char* optstring = "w:m:";
+
+    while((ret = getopt(argc, argv, optstring)) != -1) {
+        switch(ret) {
+            case 'w': w = atoi(optarg); w_flag = 1; break;
+            case 'm': m = atoi(optarg); m_flag = 1; break;
+            case '?': return -1;
+            default: abort();
+        }
+    }
+
+    if (w_flag == 0 || m_flag == 0) {
+        fprintf(stderr, "[Error]\nTwo flags are needed!\n"); 
+        return -1;
+    }
+
+
+    struct sigaction sa_child;
+    sigemptyset(&sa_child.sa_mask);
+    sa_child.sa_sigaction = sigchild_handler;
+    sa_child.sa_flags = SA_SIGINFO;
+    sigaction(SIGCHLD, &sa_child, NULL);
+
+    struct sigaction sa_sigint;
+    sigemptyset(&sa_sigint.sa_mask);
+    sa_sigint.sa_sigaction = sigint_handler;
+    sa_sigint.sa_flags = SA_SIGINFO;
+    sigaction(SIGINT, &sa_sigint, NULL);
+
+    time_t t; 
+    struct tm *_tm;
+
+    while(loop) {
+        pid_t proc = fork();
+
+        if (proc < 0) {
+            fprintf(stderr, "Something went wrong with creating process...\n");
+            exit(0);
+
+        } else if (proc == 0) {
+            srand(getpid());
+            struct sigaction sa_alarm;
+            sigemptyset(&sa_alarm.sa_mask);
+            sa_alarm.sa_sigaction = sigalarm_handler;
+            sa_alarm.sa_flags = SA_SIGINFO;
+            sigaction(SIGALRM, &sa_alarm, NULL);
+
+            int ttl = rand() % m + 1;
+
+            time(&t);
+            _tm = localtime(&t);
+            alarm(ttl);
+
+            printf("[ %d ] [ %d ] [ %02d / %02d / %04d %02d:%02d:%02d ]\n", getpid(), ttl, 
+                    _tm->tm_mday, _tm->tm_mon + 1, _tm->tm_year + 1900, _tm->tm_hour, _tm->tm_min, _tm->tm_sec
+                );
+
+            unsigned long long res = inf_factorial();
+            exit(ttl);
+        } else {
+            number_of_process++;
+        }
+        sleep(w);
+    } 
+
+    while(number_of_process > 0) {
+        pause();
+    }
+    return 0;
+}
