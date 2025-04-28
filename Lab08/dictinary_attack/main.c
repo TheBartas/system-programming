03b47d5f6274a5a0f33dc8d5cb6b740c7c485915
Bartek <bartekszewczyk2003@gmail.com>
2025-04-29 01:02:47 +0200

    Dodać wątki i benchmark.

--- Lab08/dictinary_attack/main.c
+++ Lab08/dictinary_attack/main.c
@@ -5,6 +5,10 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <crypt.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #define ERROR_NOARG -2
 #define ERROR_FPROC -3
 #define ERROR_SIZPROC -4
@@ -13,15 +17,44 @@
 #define SIZE_TTD 10000
 #define SIZE_OHTD 100000
 
-char *_mmap_rd(char *file_name, struct stat st) {
+const char *rl_res = "$6$5MfvmFOaDU$CVt7jU9wJRYz3K98EklAJqp8RMG5NvReUSVK7ctVvc2VOnYVrvyTfXaIgHn2xQS78foEJZBq2oCIqwfdNp.2V1";
+
+
+int *mem_blocks(char *mem_file, size_t size, int t) {
+    int last_block = size % t;
+    printf("Last block:         %d [ size ]\n", last_block);
+    int regular_block = (size - last_block) / t;
+    printf("Regular block:      %d [ size ]\n", regular_block);
+
+    printf("\n\n");
+
+    int *idx_arr = NULL;
+    idx_arr = malloc(sizeof * idx_arr * t);
+
+    int k = 0;
+    for (int i = regular_block - 1; i < size; i += regular_block - 1) {
+        if (mem_file[i] == '\r') { 
+            idx_arr[k] = i; 
+            k++;
+            continue; 
+        }
+        int a = i;
+        while (a < size && mem_file[a] != '\r') a++;
+        idx_arr[k] = a;
+        k++;
+    }
+    return idx_arr;
+}
+
+char *_mmap_rd(char *file_name, struct stat *st) {
     int fd = open(file_name, O_RDONLY, S_IRUSR);
-    if (fstat(fd, &st) == -1) {
+    if (fstat(fd, st) == -1) {
         fprintf(stderr, "Error: cannot get file size. File <name> [%s]\n", file_name);
         return NULL;
     }
 
     char *mem_file = NULL;
-    if ((mem_file = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == NULL) {
+    if ((mem_file = mmap(NULL, st->st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == NULL) {
         fprintf(stderr, "Error: mmap file <name> [%s]\n", file_name);
         return NULL;
     }
@@ -68,22 +101,52 @@ int main(int argc, char *argv[]) {
         }
 
         struct stat st;
-        char *mem_file = _mmap_rd(f, st);
+        char *mem_file = _mmap_rd(f, &st);
+        printf("\nSize: %ld\n", st.st_size);
 
-        for (int i = 0;i< SIZE_TD;i++) {
-            if (mem_file[i] == '\r') {
-                printf("\n");
+        printf("\n");
+
+        int *idx_arr = mem_blocks(mem_file, st.st_size, t);
+
+        char *encrpt = NULL; 
+        struct crypt_data _crypt_data;
+
+        char *buf = NULL;
+        size_t sbf = 0;
+        FILE *stream = open_memstream(&buf, &sbf);
+        bool is_found = false;
+
+        for (int i = 0; i < st.st_size; i++) {
+            if (mem_file[i] == '\r' && mem_file[i + 1] == '\n') { // chodzi o to, że hasła w pliku kończą się jako \r\n
+                i++;
+                fclose(stream);
+                if ((encrpt = crypt_r(buf, "$6$5MfvmFOaDU$", &_crypt_data)) != NULL) {
+                    if (strcmp(encrpt, rl_res) == 0) {
+                        is_found = true;
+                        break;
+                    }
+                }
+                free(buf);
+                sbf = 0;
+                stream = open_memstream(&buf, &sbf);   
                 continue;
             }
-            printf("%c", mem_file[i]);
+            fputc(mem_file[i], stream);
         }
-        
-        printf("%ld", st.st_nlink);
 
+        if (!is_found) {
+            fclose(stream);
+            if ((encrpt = crypt_r(buf, "$6$5MfvmFOaDU$", &_crypt_data)) != NULL) {
+                if (strcmp(encrpt, rl_res) == 0) {
+                    is_found = true;
+                }
+            }
+        }
 
+        if (is_found) printf("Password found! Result is \"%s\"!\n", buf);
+        free(buf);
+        free(idx_arr);
     }
-
-
 
     return 0;
 }
